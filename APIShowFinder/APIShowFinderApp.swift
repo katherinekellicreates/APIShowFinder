b44812eb6268801efd22f9966ca8462d91828656
Katherine Kelli <katherinekelli.creates@gmail.com>
2026-03-12 10:55:12 -0500

    Initial Commit

---
+++ APIShowFinder/APIShowFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIShowFinderApp.swift
+//  APIShowFinder
+//
+//  Created by Katherine Pizzirusso on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct APIShowFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
